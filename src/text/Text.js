3d2900e6150dfdd2d3101013f4ec58842e0dfaa3
Binoy Patel <binoypatel14@gmail.com>
2017-04-02 02:39:50 -0400

    Use ES6 spread forf rest of the props
    In reference to #247

--- src/text/Text.js
+++ src/text/Text.js
@@ -3,26 +3,7 @@ import { Text, StyleSheet, Platform } from 'react-native'
 import fonts from '../config/fonts'
 import normalize from '../helpers/normalizeText'
 
-let styles = {}
-
-const TextElement = ({style, children, h1, h2, h3, h4, h5, h6, fontFamily}) => (
-  <Text
-    style={[
-      styles.text,
-      h1 && {fontSize: normalize(40)},
-      h2 && {fontSize: normalize(34)},
-      h3 && {fontSize: normalize(28)},
-      h4 && {fontSize: normalize(22)},
-      h1 && styles.bold,
-      h2 && styles.bold,
-      h3 && styles.bold,
-      h4 && styles.bold,
-      fontFamily && {fontFamily},
-      style && style
-    ]}>{children}</Text>
-)
-
-styles = StyleSheet.create({
+const styles = StyleSheet.create({
   text: {
     ...Platform.select({
       android: {
@@ -37,6 +18,40 @@ styles = StyleSheet.create({
       }
     })
   }
-})
+});
+
+const TextElement = (props) => {
+  const {
+    style,
+    children,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    fontFamily,
+    ...rest,
+  } = props;
+
+  return (
+    <Text
+      style={[
+        styles.text,
+        h1 && {fontSize: normalize(40)},
+        h2 && {fontSize: normalize(34)},
+        h3 && {fontSize: normalize(28)},
+        h4 && {fontSize: normalize(22)},
+        h1 && styles.bold,
+        h2 && styles.bold,
+        h3 && styles.bold,
+        h4 && styles.bold,
+        fontFamily && {fontFamily},
+        style && style
+      ]}
+      {...rest}
+    >{children}</Text>
+  );
+}
 
 export default TextElement
